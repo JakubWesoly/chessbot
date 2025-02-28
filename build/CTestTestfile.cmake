07e6a843199dfdad8588effa79320039b62e92a5
JakubWesoly <84246305+JakubWesoly@users.noreply.github.com>
2025-02-28 02:52:12 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Kuba/dev/chessbot
-# Build directory: C:/Users/Kuba/dev/chessbot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/Kuba/dev/chessbot/build/ChessbotTests[1]_include.cmake")
-subdirs("_deps/googletest-build")
