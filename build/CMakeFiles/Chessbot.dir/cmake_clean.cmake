d3eaa1793366375b6363fae061905c3c08c28f46
JakubWesoly <jakub.wesoly@outlook.com>
2025-02-28 02:51:17 +0100

    Code style: Renamed camelCase to snake_case in Board.hpp

---
+++ build/CMakeFiles/Chessbot.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chessbot.dir/src/Board.cpp.obj"
+  "CMakeFiles/Chessbot.dir/src/Board.cpp.obj.d"
+  "CMakeFiles/Chessbot.dir/src/Brain.cpp.obj"
+  "CMakeFiles/Chessbot.dir/src/Brain.cpp.obj.d"
+  "CMakeFiles/Chessbot.dir/src/Menu.cpp.obj"
+  "CMakeFiles/Chessbot.dir/src/Menu.cpp.obj.d"
+  "CMakeFiles/Chessbot.dir/src/Move.cpp.obj"
+  "CMakeFiles/Chessbot.dir/src/Move.cpp.obj.d"
+  "CMakeFiles/Chessbot.dir/src/Program.cpp.obj"
+  "CMakeFiles/Chessbot.dir/src/Program.cpp.obj.d"
+  "CMakeFiles/Chessbot.dir/src/main.cpp.obj"
+  "CMakeFiles/Chessbot.dir/src/main.cpp.obj.d"
+  "Chessbot.exe"
+  "Chessbot.exe.manifest"
+  "Chessbot.pdb"
+  "libChessbot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chessbot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ChessbotTests.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChessbotTests.dir/src/Board.cpp.obj"
+  "CMakeFiles/ChessbotTests.dir/src/Board.cpp.obj.d"
+  "CMakeFiles/ChessbotTests.dir/src/Brain.cpp.obj"
+  "CMakeFiles/ChessbotTests.dir/src/Brain.cpp.obj.d"
+  "CMakeFiles/ChessbotTests.dir/src/Menu.cpp.obj"
+  "CMakeFiles/ChessbotTests.dir/src/Menu.cpp.obj.d"
+  "CMakeFiles/ChessbotTests.dir/src/Move.cpp.obj"
+  "CMakeFiles/ChessbotTests.dir/src/Move.cpp.obj.d"
+  "CMakeFiles/ChessbotTests.dir/src/Program.cpp.obj"
+  "CMakeFiles/ChessbotTests.dir/src/Program.cpp.obj.d"
+  "CMakeFiles/ChessbotTests.dir/tests/BoardTests.cpp.obj"
+  "CMakeFiles/ChessbotTests.dir/tests/BoardTests.cpp.obj.d"
+  "ChessbotTests.exe"
+  "ChessbotTests.exe.manifest"
+  "ChessbotTests.pdb"
+  "ChessbotTests[1]_tests.cmake"
+  "libChessbotTests.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChessbotTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
